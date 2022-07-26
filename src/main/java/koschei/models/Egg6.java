26eba2f5528f2b858af20a1a8a34525316b3b784
Soslan03 <105044227+Soslan03@users.noreply.github.com>
2022-07-26 21:31:58 +0300

    sp2

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,12 @@
 package koschei.models;
 
-public class Egg6 {
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class Egg6 {
+    @Autowired
+    private Needle7 needle7;
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle7.toString();
     }
 }
